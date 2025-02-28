d534f65b9b1eb940a704c214575bfb1c270e21f7
MD. SAMIUL ALIM <samiulpust25@gmail.com>
2025-02-28 23:40:19 +0600

    Add files via upload

---
+++ Binary Search.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int key) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == key) {
+            return mid; // Return the index if key is found
+        }
+
+        if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1; // Return -1 if key is not found
+}
+
+int main() {
+    int n, key;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the sorted elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, 0, n - 1, key);
+
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found in the array\n");
+    }
+
+    return 0;
+}
